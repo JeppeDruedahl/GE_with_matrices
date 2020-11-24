3f53bee2f5ca6279a3710ac903722508781a8d75
Jeppe Druedahl <gmf123@ku.dk>
2020-11-24 16:52:32 +0100

    Prepare for test of Omega

--- GEModel.py
+++ GEModel.py
@@ -47,11 +47,11 @@ class GEModelClass(ModelClass):
         # d. income parameters
         par.rho = 0.97 # AR(1) parameter
         par.sigma_e = 0.25 # std. of persistent shock
-        par.Ne = 11 # number of states
+        par.Ne = 5 # number of states
 
         # f. grids         
         par.a_max = 200.0 # maximum point in grid for a
-        par.Na = 1000 # number of grid points
+        par.Na = 500 # number of grid points
 
         # g. misc.
         par.simN = 1_000_000 # number of households in MC simulation
@@ -75,6 +75,7 @@ class GEModelClass(ModelClass):
         sol.a = np.zeros(sol_shape)
         sol.m = np.zeros(sol_shape)
         sol.c = np.zeros(sol_shape)
+        sol.V = np.zeros(sol_shape)
         sol.Va = np.zeros(sol_shape)
 
         # b. simulation
@@ -112,15 +113,17 @@ class GEModelClass(ModelClass):
         sol.a = 0.90*sol.m # pure guess
         sol.c = sol.m - sol.a 
         sol.Va = (1+r)*sol.c**(-par.sigma) if Va is None else Va
+        sol.V = sol.c**(1-par.sigma)/(1-par.sigma)
 
         it = 0
         while True:
 
             # i. save
             a_old = sol.a.copy()
+            V_old = sol.V.copy()
 
             # ii. solve backwards
-            solve_backwards(par,r,w,sol.Va,sol.Va,sol.a,sol.c,sol.m)
+            solve_backwards(par,r,w,sol.Va,sol.Va,sol.a,sol.c,sol.m,sol.V,sol.V)
 
             # iii. check
             if np.max(np.abs(sol.a-a_old)) < par.solve_tol: break
@@ -131,6 +134,7 @@ class GEModelClass(ModelClass):
 
         if do_print:
             print(f'household problem solved in {elapsed(t0)} [{it} iterations]')
+            print(f'[value function abs. max diff. is {np.max(np.abs(sol.V-V_old)):.8f}]')
 
     def simulate(self,seed=1917,do_print=False):
         """ simulate the model """
@@ -175,12 +179,17 @@ class GEModelClass(ModelClass):
 ######################
 
 @njit(parallel=True)        
-def solve_backwards(par,r,w,Va_p,Va,a,c,m):
+def solve_backwards(par,r,w,Va_p,Va,a,c,m,V_p,V):
     """ perform time iteration step with Va_p from previous iteration """
 
     # a. post-decision
-    marg_u_plus = (par.beta*par.e_trans)@Va_p #@ matrix multiplication
-    print(np.shape(marg_u_plus), np.shape(par.e_trans),np.shape(Va_p))
+    marg_u_plus = (par.beta*par.e_trans)@Va_p
+    Vbar = (par.beta*par.e_trans)@V_p
+        
+    # convert from domain ]-\infty:0[ to ]0:infty[
+    # -> extrapolation does not break domain does
+
+    inv_Vbar = -1.0/Vbar 
 
     # b. egm loop
     for i_e in prange(par.Ne):
@@ -196,7 +205,13 @@ def solve_backwards(par,r,w,Va_p,Va,a,c,m):
 
         # iii. envelope condition
         Va[i_e] = (1+r)*c[i_e]**(-par.sigma)
-    
+
+        # iv. value function
+        for i_a in range(par.Na):
+            inv_Vbar_now = linear_interp.interp_1d(par.a_grid,inv_Vbar[i_e],a[i_e,i_a])
+            Vbar_now = -inv_Vbar_now # convert back
+            V[i_e,i_a] = c[i_e,i_a]**(1-par.sigma)/(1-par.sigma) + Vbar_now
+
 @njit(parallel=True)        
 def simulate_forwards(par,sol,r,w,i_e,y,a):
     """ simulate forward """
